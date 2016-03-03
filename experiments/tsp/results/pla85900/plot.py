87a3b5d454f4d2963f14a9e382d5bb34443e2152
phrb <pedro.bruel@gmail.com>
2016-03-03 11:04:37 -0300

    Revert deletion of results.

--- experiments/tsp/results/pla85900/plot.py
+++ experiments/tsp/results/pla85900/plot.py
@@ -11,48 +11,31 @@ plt.rc('text', usetex = True)
 plt.rc('font', family = 'serif')
 
 font = {'family' : 'serif',
-        'size'   : 18}
+        'size'   : 14}
 
 mpl.rc('font', **font)
 
-# StochasticSearch Seq
-ss_path           = "jl//15min/seq/"
-ss_data           = []
-ss_sample_run     = [[], []]
-
-# StochasticSearch Par
-ss_par_path       = "jl//15min/par/"
-ss_par_data       = []
-ss_par_sample_run = [[], []]
+# StochasticSearch Data
+ss_path       = "jl/15min/1w_smm/"
+ss_data       = []
+ss_sample_run = [[], []]
 
 # OpenTuner Data
-ot_path           = "py/15min/"
-ot_data           = []
-ot_sample_run     = [[], []]
+ot_path       = "py/15min/"
+ot_data       = []
+ot_sample_run = [[], []]
 
 for run in os.listdir(ss_path):
     with open(ss_path + run + "/last.txt") as file:
         last = float(file.read().rstrip("\n").split(" ")[1])
         ss_data.append(last)
 
-with open(ss_path + "run_2/best.txt") as file:
+with open(ss_path + "run_4/best.txt") as file:
     text_points = file.read().splitlines()
     for line in text_points:
         point = line.split(" ")
         ss_sample_run[0].append(float(point[0]))
         ss_sample_run[1].append(float(point[1]))
-
-for run in os.listdir(ss_par_path):
-    with open(ss_par_path + run + "/last.txt") as file:
-        last = float(file.read().rstrip("\n").split(" ")[1])
-        ss_par_data.append(last)
-
-with open(ss_par_path + "run_1/best.txt") as file:
-    text_points = file.read().splitlines()
-    for line in text_points:
-        point = line.split(" ")
-        ss_par_sample_run[0].append(float(point[0]))
-        ss_par_sample_run[1].append(float(point[1]))
 
 for run in os.listdir(ot_path):
     with open(ot_path + run + "/best.txt") as file:
@@ -66,7 +49,7 @@ with open(ot_path + "run_1/best.txt") as file:
         ot_sample_run[0].append(float(point[0]))
         ot_sample_run[1].append(float(point[1]))
 
-boxplot_data = [ss_data, ot_data, ss_par_data]
+boxplot_data = [ss_data, ot_data]
 
 fig = plt.figure(1, figsize=(9, 6))
 
@@ -78,7 +61,7 @@ plt.setp(bp['boxes'], color='black')
 plt.setp(bp['whiskers'], color='darkgray')
 plt.setp(bp['fliers'], color='red', marker='+')
 
-ax.set_xticklabels(["StochasticSearch.jl (seq)", "OpenTuner", "StochasticSearch.jl (p = 4)"])
+ax.set_xticklabels(["StochasticSearch.jl", "OpenTuner"])
 
 # Add a horizontal grid to the plot, but make it very light in color
 # so we can use it for reading data values but not be distracting
@@ -89,25 +72,22 @@ ax.set_title("TSP Solution (85900 Cities) Cost After Tuning for 15 minutes (4 ru
 ax.set_xlabel("Tuner")
 ax.set_ylabel("Solution Cost")
 
-fig.savefig('par_pla85900_15min_comparison.eps', format = 'eps', dpi = 1000)
+fig.savefig('pla85900_15min_comparison.eps', format = 'eps', dpi = 1000)
 
 plt.clf()
 
 fig = plt.figure(1, figsize=(9, 6))
 ax = fig.add_subplot(111)
 
-ax.set_xlim([-10, max(max(ss_sample_run[0]), max(ot_sample_run[0]), max(ss_par_sample_run[0])) + 10])
+ax.set_xlim([-10, max(max(ss_sample_run[0]), max(ot_sample_run[0])) + 10])
 
-ax.set_ylim([min(min(ss_sample_run[1]), min(ot_sample_run[1]), min(ss_par_sample_run[1])) - 400000, max(max(ss_sample_run[1]), max(ot_sample_run[1]), max(ss_par_sample_run[1])) + 400000])
+ax.set_ylim([min(min(ss_sample_run[1]), min(ot_sample_run[1])) - 400000, max(max(ss_sample_run[1]), max(ot_sample_run[1])) + 400000])
 
 ss_b = ax.scatter(ss_sample_run[0], ss_sample_run[1], marker = 'x', color = 'c')
 ax.plot(ss_sample_run[0], ss_sample_run[1], color = 'c')
 
 ot_b = ax.scatter(ot_sample_run[0], ot_sample_run[1], marker = 'o', color = 'g')
 ax.plot(ot_sample_run[0], ot_sample_run[1], color = 'g')
-
-ss_par_b = ax.scatter(ss_par_sample_run[0], ss_par_sample_run[1], marker = 'x', color = 'b')
-ax.plot(ss_par_sample_run[0], ss_par_sample_run[1], color = 'b')
 
 ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey',
                       alpha=0.5)
@@ -116,8 +96,8 @@ ax.set_title("Best TSP Solution (85900 Cities) during a Tuning Run")
 ax.set_xlabel("Tuning Time")
 ax.set_ylabel("Solution Cost")
 
-plt.legend((ss_b, ot_b, ss_par_b),
-           ('StochasticSearch.jl (seq)', 'OpenTuner', 'StochasticSearch.jl (p = 4)'),
-           prop = {'size' : 14})
+plt.legend((ss_b, ot_b),
+           ('StochasticSearch.jl', 'OpenTuner'),
+           prop = {'size' : 12})
 
-fig.savefig('par_pla85900_15min_best_comparison.eps', format = 'eps', dpi = 1000)
+fig.savefig('pla85900_15min_best_comparison.eps', format = 'eps', dpi = 1000)
